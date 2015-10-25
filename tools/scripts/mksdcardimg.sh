d61cd098780c5af2ac410eae0d2a2734d1e0b992
Pascal Frehner <frehnerp@gmail.com>
2015-10-25 16:32:28 +0100

    get changes working

--- tools/scripts/mksdcardimg.sh
+++ tools/scripts/mksdcardimg.sh
@@ -34,7 +34,7 @@ EOF
 
 #sudo dd if=u-boot-sd.img of=$IMG conv=notrunc,sync seek=64
 sudo dd if=rk_loader/${SDBOOT_IMG} of=$IMG skip=1 seek=1
-sudo dd if=parameter.img of=$IMG conv=notrunc,sync seek=$OFFSET_LOADER
+sudo dd if=parameter.img of=$IMG conv=notrunc,sync seek=$(($OFFSET_LOADER))
 sudo dd if=boot/boot-linux.img of=$IMG conv=notrunc,sync seek=$(($OFFSET_LOADER+$OFFSET_BOOT))
 sudo dd if=rootfs.img of=$IMG conv=notrunc,sync seek=$ROOTFS_SECTOR
 echo -e "Image is at \033[1;36m$(pwd)/${IMG}\033[00m"
